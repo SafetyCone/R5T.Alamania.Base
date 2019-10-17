d9bbf163d97d73b704a663b27007bb515d8d6419
MinexAutomation <minex.automation@gmail.com>
2019-10-17 00:59:04 -0700

    Initial commit of source-stub.

---
+++ source/R5T.Alamania.Base.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Alamania.Base.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Alamania.Base/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Alamania.Base
+{
+    public class Class1
+    {
+    }
+}
