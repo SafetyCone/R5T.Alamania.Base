39069e406a519f0e7345aa4590aed05f28e9daa1
davidcoats <david@getsafetycone.com>
2022-04-27 15:24:53 -0700

    Interim changes.

---
+++ source/R5T.Alamania.Base/Code/Services/Definitions/IRivetOrganizationDirectoryPathProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Alamania
+{
+    [ServiceDefinitionMarker]
+    public interface IRivetOrganizationDirectoryPathProvider : IServiceDefinition
+    {
+        string GetRivetOrganizationDirectoryPath();
+    }
+}
